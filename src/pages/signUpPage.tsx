08321bc02279a366552d880c35bb1e3e199e0587
Kavindu Hansana <hansana2024@gmail.com>
2025-05-03 23:44:24 +0530

    refactor SignUpPage component to consolidate Suspense usage

--- src/pages/signUpPage.tsx
+++ src/pages/signUpPage.tsx
@@ -8,12 +8,13 @@ const SignUp = lazy(() => import('../SignUpLogin/SignUp')); // Lazy load the Sig
 const SignUpPage = () => {
     const location = useLocation()
     return (
+        <Suspense fallback={<div>Loading...</div>}>
         <div className="min-h-[90vh] bg-gray-100 font-['poppins'] overflow-hidden">
             <div className={`not-first:w-[100vw] h-[100vh] transition-all ease-in-out duration-1000 flex [&>*]:flex-shrink-0 ${location.pathname == '/signup' ? '-translate-x-1/2' : 'transate-x-0'}`}>
                 {/* <Login /> */}
-                <Suspense fallback={<div>Loading...</div>}>
+                
                     <Login />
-                </Suspense>
+                
                 <div className={`w-1/2 h-full transition-all duration-1000 ease-in-out ${location.pathname == "/signup" ? "rounded-r-[200px]" : "rounded-l-[200px]"} bg-blue-500 flex flex-col items-center justify-center gap-5`}>
                     <div className='flex items-center gap-2'>
                         <div className='text-5xl font-semibold text-white'>Uni Connect</div>
@@ -21,11 +22,12 @@ const SignUpPage = () => {
                     <div className="text-2xl font-semibold">Find job to Start Your jerney</div>
                 </div>
                 {/* <SignUp /> */}
-                <Suspense fallback={<div>Loading...</div>}>
+                
                     <SignUp />
-                </Suspense>
+                
             </div>
         </div>
+        </Suspense>
     )
 }
 
