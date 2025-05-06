9628a5efb3a7ae91926c322fb69804b3c4d1eb2c
Kavindu Hansana <hansana2024@gmail.com>
2025-05-07 02:03:10 +0530

    implement ProfileMenu component

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,29 +1,20 @@
 
-import { Avatar, Indicator } from '@mantine/core';
+import { Indicator } from '@mantine/core';
 import { Settings , Bell } from 'tabler-icons-react';
-import avatarImage from '../assets/avatar.png';
 import NavLinks from './NavLinks';
+import ProfileMenu from './ProfileMenu';
 
 const Header = () => {
     return <div className="w-full bg-gray-900 px-6 h-25 text-white p-4 flex justify-between items-center">
         
-        <div className='flex items-center gap-2'>
+        <div className='flex items-center gap-2 cursor-pointer'>
             <div className='text-2xl font-semibold text-blue-500'>Uni Connect</div>
         </div>
 
         {NavLinks()}
 
         <div className='flex gap-5 items-center justify-around'>           
-            <div className='flex items-center gap-2'>
-                <div>Kavindu Hansana</div>
-                <Indicator inline size={11} offset={4} position="bottom-end" color="green" withBorder>
-                    <Avatar
-                        size={35}
-                        src={avatarImage}
-                        alt="profile photo"
-                    />
-                </Indicator>
-            </div>
+            <ProfileMenu/>
             <div className='bg-gray-900 p-1.5 rounded-full'>
                 <Settings size={25}/>
             </div>
---
+++ src/layouts/ProfileMenu.tsx
@@ -0,0 +1,69 @@
+import { Menu, Avatar, Indicator, Switch } from '@mantine/core';
+import avatarImage from '../assets/avatar.png';
+import {
+    IconMessageCircle,
+    IconUserCircle,
+    IconFileText,
+    IconMoon,
+    IconMoonStars,
+    IconSun,
+    IconLogout2,
+} from '@tabler/icons-react';
+import { useState } from 'react';
+
+const ProfileMenu = () => {
+    const [checked, setChecked] = useState(false);
+    const [opened, setOpened] = useState(false);
+    return (
+        <Menu shadow="md" width={200} opened={opened} onChange={setOpened}>
+            <Menu.Target>
+                <div className='flex items-center gap-2 cursor-pointer'>
+                    <div>Kavindu Hansana</div>
+                    <Indicator inline size={11} offset={4} position="bottom-end" color="green" withBorder>
+                        <Avatar
+                            size={35}
+                            src={avatarImage}
+                            alt="profile photo"
+                        />
+                    </Indicator>
+                </div>
+            </Menu.Target>
+
+            <Menu.Dropdown onChange={()=>setOpened(true)}>
+                <Menu.Item leftSection={<IconUserCircle size={14} />}>
+                    Profile
+                </Menu.Item>
+                <Menu.Item leftSection={<IconMessageCircle size={14} />}>
+                    Messages
+                </Menu.Item>
+                <Menu.Item leftSection={<IconFileText size={14} />}>
+                    Resume
+                </Menu.Item>
+                <Menu.Item
+                    leftSection={<IconMoon size={14} />}
+                    rightSection={
+                        <Switch checked={checked}
+                            onChange={(event) => setChecked(event.currentTarget.checked)}
+                            size="md"
+                            color="dark.4"
+                            onLabel={<IconSun size={16} stroke={2.5} color="var(--mantine-color-yellow-4)" />}
+                            offLabel={<IconMoonStars size={16} stroke={2.5} color="var(--mantine-color-blue-6)" />}
+                        />
+                    }
+                >
+                    Dark mode
+                </Menu.Item>
+
+                <Menu.Divider />
+                <Menu.Item
+                    color="red"
+                    leftSection={<IconLogout2 size={14} />}
+                >
+                    Logout
+                </Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
+    );
+}
+
+export default ProfileMenu;
