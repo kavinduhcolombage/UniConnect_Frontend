a74c2907d9bb86fd739cd465a8fcade3687010ab
Kavindu Hansana <hansana2024@gmail.com>
2025-05-06 13:16:57 +0530

    add experiance and certification card for profile

--- src/pages/TalentProfilePage.tsx
+++ src/pages/TalentProfilePage.tsx
@@ -5,21 +5,24 @@ import Header from "../layouts/Header"
 import { Button } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
 import Profile from "../TalentProfile/Profile";
+import { profile } from "../Data/TalentData";
+import RecommendTalent from "../TalentProfile/RecomendedTalent";
 
 const TalentProfilePage = () => {
     return (
-        <div className="bg-gray-100 min-h-[100vh] font-['poppins'] p-4">
+        <div className="bg-gray-100 min-h-[100vh] font-['poppins']">
             <Header />
             <Link className="my-4 inline-block" to="/find-talents">
-                <Button  leftSection={<IconArrowLeft size={20}/>} className="!text-blue-700" variant="light">Back</Button>
+                <Button leftSection={<IconArrowLeft size={20} />} className="!text-blue-700" variant="light">Back</Button>
             </Link>
 
-            <div className="flex gap-5">
-        <Profile/>
+            <div className="flex gap-5 p-4">
+                <Profile {...profile} />
+                <RecommendTalent />
             </div>
             <Footer />
         </div>
-        
+
     )
 }
 
