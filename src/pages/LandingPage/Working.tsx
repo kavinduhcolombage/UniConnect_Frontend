1ad03190f491e04a0ab57f62de223390bb6e9208
Kavindu Hansana <hansana2024@gmail.com>
2025-05-07 04:16:48 +0530

    enhance landing page

--- src/pages/LandingPage/Working.tsx
+++ src/pages/LandingPage/Working.tsx
@@ -5,34 +5,34 @@ const Working = () => {
 
         <div className="flex px-16 justify-between items-center">
             <div className="">
-                <img className="w-[30rem]" src="/google.png" alt="working photo" />
+                <img className="w-[30rem]" src="/images/boy.jpg" alt="working photo" />
             </div>
             <div className="flex flex-col gap-5 w-[60%]">
                 <div className="flex items-center gap-4">
                     <div className="p-2.5 bg-blue-300 rounded-full">
-                        <img className="h-12 w-12" src="/google.png" alt="build resume" />
+                        <img className="h-12 w-18" src="/images/Resume.png" alt="build resume" />
                     </div>
                     <div>
-                        <div className="text-red-600 text-xl font-semibold">Build your resume</div>
-                        <div className="text-purple-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eveniet fugit, placeat enim asperiores quasi est commodi nesciunt</div>
+                        <div className="text-blue-700 text-xl font-semibold">Build your resume</div>
+                        <div>Create a professional resume that highlights your skills, experience, and achievements to stand out to employers.</div>
                     </div>
                 </div>
                 <div className="flex items-center gap-4">
                     <div className="p-2.5 bg-blue-300 rounded-full">
-                        <img className="h-12 w-12" src="/google.png" alt="build resume" />
+                        <img className="h-12 w-18" src="/images/job.png" alt="job" />
                     </div>
                     <div>
-                        <div className="text-red-600 text-xl font-semibold">Get Hired</div>
-                        <div className="text-purple-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eveniet fugit, placeat enim asperiores quasi est commodi nesciunt</div>
+                        <div className="text-blue-700 text-xl font-semibold">Apply for job</div>
+                        <div>Search and apply for your dream job using our user-friendly platform with thousands of active listings.</div>
                     </div>
                 </div>
                 <div className="flex items-center gap-4">
                     <div className="p-2.5 bg-blue-300 rounded-full">
-                        <img className="h-12 w-12" src="/google.png" alt="build resume" />
+                        <img className="h-12 w-18" src="/images/hired.png" alt="hired" />
                     </div>
                     <div>
-                        <div className="text-red-600 text-xl font-semibold">Apply job</div>
-                        <div className="text-purple-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eveniet fugit, placeat enim asperiores quasi est commodi nesciunt</div>
+                        <div className="text-blue-700 text-xl font-semibold">Get Hired</div>
+                        <div>Receive interview calls and secure job offers from top companies looking for talent like you.</div>
                     </div>
                 </div>
             </div>
