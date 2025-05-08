6505cf57cf01e64c72120a70eed0022af31b7eec
Kavindu Hansana <hansana2024@gmail.com>
2025-05-08 11:22:54 +0530

    refactor ProfileSlice reducers for direct state modification

--- src/Slices/ProfileSlice.tsx
+++ src/Slices/ProfileSlice.tsx
@@ -2,20 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 import { updateProfile } from "../Services/ProfileService";
 
 const ProfileSlice = createSlice({
-  //test
     name: 'profile',
     initialState: {},
     reducers: {
-      changeProfile: (state, action) => {
-        state = updateProfile(action.payload);
-        return action.payload;
-      },
-      setProfile: (state, action) => {
-        state = action.payload;
-        return state;
-      }
+        changeProfile: (state, action) => {
+            // Directly modify the state
+            Object.assign(state, updateProfile(action.payload));
+        },
+        setProfile: (state, action) => {
+            // Directly modify the state
+            Object.assign(state, action.payload);
+        }
     }
-  });
-  
-  export const { changeProfile, setProfile } = ProfileSlice.actions;
-  export default ProfileSlice.reducer;
+});
+
+export const { changeProfile, setProfile } = ProfileSlice.actions;
+export default ProfileSlice.reducer;
