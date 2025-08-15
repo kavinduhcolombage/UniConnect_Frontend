09fa07a8036c10c447ec4647adf44a6388ad183a
Kavindu Hansana <hansana2024@gmail.com>
2025-08-16 00:19:04 +0530

    feat: enhance session management and error handling in Axios interceptor

--- src/Interceptor/AxiosInterceptor.tsx
+++ src/Interceptor/AxiosInterceptor.tsx
@@ -1,6 +1,12 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 import Host_Url from "../Services/HostUrl";
+import { removeUser } from "../Slices/UserSlice";
+import { removejwt } from "../Slices/JwtSlice";
+import { notifications } from '@mantine/notifications';
+import { IconX } from "@tabler/icons-react";
+
+let sessionExpiredNotified = false;
 
 const axiosInstance = axios.create({
     baseURL: `${Host_Url}`
@@ -19,14 +25,31 @@ axiosInstance.interceptors.request.use(
     }
 )
 
-export const setupResponseInterceptor = (navigate: any) => {
+export const setupResponseInterceptor = (navigate: any, dispatch: any) => {
     axiosInstance.interceptors.response.use(
         (response) => {
+            sessionExpiredNotified = false;
             return response;
         },
         (error) => {
             if (error.response?.status === 401) {
-                navigate('/login');
+                if (!sessionExpiredNotified) {
+                    sessionExpiredNotified = true;
+                    dispatch(removeUser());
+                    dispatch(removejwt());                    
+                    notifications.show({
+                        position: 'top-center',
+                        withCloseButton: true,
+                        onClose: () => navigate('/login'),
+                        onOpen: () => console.log('mounted'),
+                        autoClose: 5000,
+                        title: "Session Expired",
+                        message: 'Your session has expired. Please log in again.',
+                        color: 'red',
+                        icon: <IconX />,
+                        loading: false
+                    });
+                }
             }
             return Promise.reject(error);
         }
